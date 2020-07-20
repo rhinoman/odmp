c6714d738163ba3867d9f1c86c1db200cb02528c
James Adam <james@jamesadam.me>
2020-07-20 07:18:18 -0400

    removed css from lein clean targets in ui

--- opendmp-ui/project.clj
+++ opendmp-ui/project.clj
@@ -28,8 +28,7 @@
 
   :source-paths ["src/cljs"]
 
-  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"
-                                    "resources/public/css"]
+  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
 
   :shell {:commands {"open" {:windows ["cmd" "/c" "start"]
                              :macosx  "open"
