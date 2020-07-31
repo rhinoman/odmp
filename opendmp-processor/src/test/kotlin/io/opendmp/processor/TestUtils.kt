39537ff276e25b201f716587d5ff132eb5c7de9b
James Adam <james@jamesadam.me>
2020-07-30 22:56:59 -0400

    working on tests for processor

---
+++ opendmp-processor/src/test/kotlin/io/opendmp/processor/TestUtils.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2020. The Open Data Management Platform contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opendmp.processor
+
+import io.opendmp.common.model.ProcessorRunModel
+import io.opendmp.common.model.ProcessorType
+import io.opendmp.common.model.SourceModel
+import io.opendmp.common.model.SourceType
+import java.util.*
+
+object TestUtils {
+
+    fun createProcessor(name: String, type: ProcessorType, inputs: List<SourceModel>) : ProcessorRunModel {
+        return ProcessorRunModel(
+                id = UUID.randomUUID().toString(),
+                flowId = UUID.randomUUID().toString(),
+                name = name,
+                inputs = inputs,
+                type = type
+        )
+    }
+
+    fun createFileIngestProcessor(name: String) : ProcessorRunModel {
+        return createProcessor(
+                name,
+                ProcessorType.INGEST,
+                listOf(SourceModel(SourceType.INGEST_FILE_DROP, "/tmp/input")))
+    }
+
+    fun createScriptProcessor(name: String, inputProcs: List<String>) : ProcessorRunModel {
+        return createProcessor(
+                name,
+                ProcessorType.SCRIPT,
+                inputProcs.map{ SourceModel(SourceType.PROCESSOR, it)}
+        )
+    }
+}
--- opendmp-processor/src/test/kotlin/io/opendmp/processor/run/TestRunPlanRouteBuilder.kt
+++ opendmp-processor/src/test/kotlin/io/opendmp/processor/run/TestRunPlanRouteBuilder.kt
@@ -16,35 +16,32 @@
 
 package io.opendmp.processor.run
 
+import io.opendmp.processor.TestUtils
 import io.opendmp.processor.config.PulsarConfig
 import io.opendmp.processor.config.RedisConfig
+import io.opendmp.processor.domain.RunPlan
 import io.opendmp.processor.handler.RunPlanRequestHandler
 import io.opendmp.processor.messaging.RunPlanRequestRouter
 import org.apache.camel.CamelContext
 import org.apache.camel.test.spring.junit5.CamelSpringBootTest
-import org.apache.camel.test.spring.junit5.CamelSpringTest
-import org.apache.camel.test.spring.junit5.CamelSpringTestSupport
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.support.AbstractApplicationContext
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import java.util.*
 
 @SpringBootTest
 @ExtendWith(SpringExtension::class)
 @CamelSpringBootTest
 @ContextConfiguration
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-class TestRunPlanRouteBuilder {
-
-    @Autowired
-    lateinit var camelContext: CamelContext
+class TestRunPlanRouteBuilder @Autowired constructor(
+        private val camelContext: CamelContext) {
 
     @MockBean
     lateinit var pulsarConfig: PulsarConfig
@@ -58,8 +55,25 @@ class TestRunPlanRouteBuilder {
     @MockBean
     lateinit var runPlanRequestRouter: RunPlanRequestRouter
 
+    fun basicRunPlan() : RunPlan {
+        val iProc = TestUtils.createFileIngestProcessor("fileIn")
+        val tProc = TestUtils.createScriptProcessor("script1", listOf(iProc.id))
+        val procs = mapOf(iProc.id to iProc, tProc.id to tProc)
+        val procDeps = mapOf(iProc.id to listOf(tProc.id))
+        return RunPlan(
+                id = UUID.randomUUID().toString(),
+                flowId = UUID.randomUUID().toString(),
+                startingProcessors = listOf(iProc.id),
+                processorDependencyMap = procDeps,
+                processors = procs
+        )
+    }
+
     @Test
     fun testSimpleRunPlan() {
+        val runPlan = basicRunPlan()
+        val routeBuilder = RunPlanRouteBuilder(runPlan)
+        camelContext.addRoutes(routeBuilder)
         assertEquals(true, true)
     }
 
