e97131f0be191025c6cda61aa23b0fe41eee0924
James Adam <james@jamesadam.me>
2020-10-28 17:21:12 -0400

    updated to keycloak 11; other library updates

--- opendmp-ui/src/cljs/deps.cljs
+++ opendmp-ui/src/cljs/deps.cljs
@@ -1,8 +1,8 @@
 {:npm-dev-deps {"shadow-cljs"           "2.8.109"}
- :npm-deps {"@material-ui/core"         "^4.10.0"
+ :npm-deps {"@material-ui/core"         "^4.11.0"
             "@material-ui/icons"        "^4.9.1"
             "@material-ui/lab"          "^4.0.0-alpha.56"
             "react-lineto"              "^3.2.0"
-            "keycloak-js"               "^10.0.2"
-            "react-ace"                 "^9.1.1"
+            "keycloak-js"               "^11.0.2"
+            "react-ace"                 "^9.2.0"
             "ace-builds"                "^1.4.12"}}
